0b2929feaf3f00f29b623938f70068a955ab5a89
Emma Dean <emmadeannow@gmail.com>
2017-07-13 08:59:16 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log(customers.js)
+console.log(customers);
+//customers.array 12 of em
+
+const container = document.querySelector('#container')
+let template = ``
+
+for (var i = 0; i < customers.results.length; i++) {
+      let data = customers.results[i];
+      console.log(data.name)
+
+const template = `
+  <article>
+  <div class="userImage">
+    <img src="${data.picture.thumbnail}">
+  </div>
+    <h2>${data.name}</h2>
+    <h3>${data.email}</h3>
+          console.log(data.name)
+    <div class="email">
+    </div>
+  </article>
+`;
+}
+
+// Insert the template onto our page
+container.innerHTML = template

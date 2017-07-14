e3048e9dbcc345be4bbda942cdf4aa77e8953180
Emma Dean <emmadeannow@gmail.com>
2017-07-13 20:57:12 -0400

    took out line styles and added to css, need to hard code the html

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-// console.log(customers);
-// //customers.array 12 of em
-//
-// const container = document.querySelector('#container')
-// let template = ``
-//
-// for (var i = 0; i < customers.results.length; i++) {
-//       let data = data;
-//       console.log(data.name)
-//
-// const template = `
-//   <article>
-//   <div class="userImage">
-//     <img src="${data.picture.thumbnail}">
-//   </div>
-//     <h2>${data.name}</h2>
-//     <h3>${data.email}</h3>
-//           console.log(data.name)
-//     <div class="email">
-//     </div>
-//   </article>
-// `;
-// }
-
+console.log(customers)
 
 for (var i = 0; i < customers.results.length; i += 1) {
 
@@ -43,15 +20,11 @@ for (var i = 0; i < customers.results.length; i += 1) {
     background-image: url(${data.picture.large});
     "></div>
 
-    <h2 style="text-transform: capitalize;
-      font-size: 20px;
-      text-decoration: underline solid green;">
-      ${data.name.first} ${data.name.last}</h2>
+    <h2>${data.name.first} ${data.name.last}</h2>
 
-    <h3 style="
-    font-weight: lighter;">${data.email}</h3>
+    <h3>${data.email}</h3>
 
-    <div style="line-height: 40%;  margin: 15% auto;">
+    <div class="addressInfo">
     <p>${data.location.street}</p>
     <p>${data.location.city}, ${data.location.state} ${data.location.postcode} </p>
     <p>${data.phone}</p>

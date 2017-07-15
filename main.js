23bd8130dc0119789d301b74ee01224c42297df5
Emma Dean <emmadeannow@gmail.com>
2017-07-15 09:47:43 -0400

    fixed the spacing back

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log(customers)
-
 for (var i = 0; i < customers.results.length; i += 1) {
 
   let customerID = document.querySelector("#customer")
@@ -20,15 +18,20 @@ for (var i = 0; i < customers.results.length; i += 1) {
     background-image: url(${data.picture.large});
     "></div>
 
-    <h2>${data.name.first} ${data.name.last}</h2>
+    <h2 style="text-transform: capitalize;
+      font-size: 20px;
+      text-decoration: underline solid green;">
+      ${data.name.first} ${data.name.last}</h2>
 
-    <h3>${data.email}</h3>
+    <h3 style="
+    font-weight: lighter;">${data.email}</h3>
 
-    <div class="addressInfo">
+    <div style="line-height: 40%;  margin: 15% auto;">
     <p>${data.location.street}</p>
     <p>${data.location.city}, ${data.location.state} ${data.location.postcode} </p>
     <p>${data.phone}</p>
-    <h4>${data.id.name}, ${data.id.value}</h4>
+    <h4 style ="
+    font-weight: bold">${data.id.name}, ${data.id.value}</h4>
     </div>
     </div>`
 
